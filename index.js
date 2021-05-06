6de742e46540c8953f4d3282ac2861557b6a1a04
Elliot Robin <mrttasarz@gmail.com>
2021-05-06 22:54:50 +0100

    Add new files

---
+++ index.js
@@ -0,0 +1,126 @@
+
+// main
+const current_money = document.getElementById('current-money')
+const current_incomes = document.getElementById('current-incomes')
+const current_expenses = document.getElementById('current-expenses')
+const delete_btn = document.querySelectorAll('delete-btn')
+const list = document.getElementById('list')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
+const add_btn = document.getElementById('add-btn')
+const add_div = document.getElementById('add-new')
+const select = document.getElementById('sort')
+
+
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+// show warning
+
+function showWarning() {
+  const warning = document.createElement('div')
+  warning.classList.add('warning')
+  warning.innerHTML = '<h3>Please fill in the reqired files!</h3>'
+  add_div.prepend(warning)
+  console.log(add_div.children)
+  setTimeout(() => {
+    add_div.removeChild(warning)
+  }, 2000)
+}
+
+// add transaction
+function addTransaction() {
+  const rand = Math.floor(Math.random() * 999)
+  if (text.value === '' || amount.value === '') {
+    showWarning()
+  } else {
+    const transaction = {
+      id: rand,
+      text: text.value,
+      amount: +amount.value
+    }
+    transactions.push(transaction)
+    addTransactionToDOM(transaction)
+    calculateValues()
+    updateLocalStorage()
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+function addTransactionToDOM(transaction) {
+  const li = document.createElement('li')
+  li.innerHTML = `<span>${transaction.text}</span> <span>${transaction.amount}</span><button type="button" class="delete-btn" onClick=deleteTransaction(${transaction.id})>X</button>`
+  list.appendChild(li)
+}
+
+function deleteTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id)
+
+  updateLocalStorage()
+  initialize()
+}
+
+// calculate
+
+function calculateValues() {
+
+  const amounts = transactions.map(transaction => transaction.amount)
+
+  const incomes = amounts
+    .filter(amount => amount > 0)
+    .reduce((acc, cv) => (acc + cv), 0)
+    .toFixed(2)
+  const expenses = amounts
+    .filter(amount => amount < 0)
+    .reduce((acc, cv) => (acc + cv), 0)
+    .toFixed(2)
+
+  const total = amounts.reduce((acc, cv) => (acc + cv), 0).toFixed(2)
+
+  current_money.innerText = `$${total}`;
+  current_incomes.innerText = `$${incomes}`;
+  current_expenses.innerText = `$${expenses}`;
+
+}
+
+function optionsFunc() {
+  if (sort.value == 'expenses') {
+    filterExpenses()
+  }
+  if (sort.value == 'incomes') {
+    filterIncomes()
+  }
+  if (sort.value == 'no-filter') {
+    initialize()
+  }
+}
+
+function filterExpenses() {
+  list.innerHTML = '';
+  const expenses = transactions.filter(transaction => transaction.amount < 0)
+  expenses.forEach(addTransactionToDOM)
+}
+function filterIncomes() {
+  list.innerHTML = '';
+  const incomes = transactions.filter(transaction => transaction.amount > 0)
+  incomes.forEach(addTransactionToDOM)
+}
+
+
+// update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+function initialize() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionToDOM)
+  calculateValues()
+}
+
+initialize()
+delete_btn.forEach(deleteTransaction)
+add_btn.addEventListener('click', addTransaction)
+select.addEventListener('change', optionsFunc)
